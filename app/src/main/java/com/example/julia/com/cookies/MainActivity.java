3bee94abce2586c96df485f77830d9b16d8b6885
nekomaneki <mjuliagirona@gmail.com>
2017-06-14 22:58:29 -0300

    first commit

---
+++ app/src/main/java/com/example/julia/com/cookies/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.julia.com.cookies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static com.example.julia.com.cookies.R.drawable.after_cookie;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+        ImageView beforeCookieImageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        beforeCookieImageView.setImageResource(after_cookie);
+
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+        TextView beforeCookieTextView = (TextView) findViewById(R.id.status_text_view);
+        beforeCookieTextView.setText("I'm so full");
+
+    }
+}
